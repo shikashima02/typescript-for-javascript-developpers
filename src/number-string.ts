64370d12fd5b263a86fd4b9944b92b64a565cdfc
鹿島隼人 <townsfolks01@gmail.com>
2022-07-01 09:06:36 +0900

    create number-string.ts, rest-parameters.ts

---
+++ src/number-string.ts
@@ -0,0 +1,10 @@
+export {};
+
+let year = 1976;
+
+console.log(year);
+
+// 43歳
+let age = 0x2b;
+
+console.log(age);
---
+++ src/rest-parameters.ts
@@ -0,0 +1,15 @@
+export {};
+
+// javascriptのreducer
+const reducer = (accumulator: number, currentValue:number) => {
+  console.log({accumulator, currentValue})
+  return accumulator + currentValue;
+}
+
+const sum: (...values: number[]) => number = (...values: number[]):number => {
+  console.log(values);
+  return values.reduce(reducer);
+}
+
+console.log(sum(1,2,3,4,5));
+// [1, 2, 3, 4].reduce(reducer)
